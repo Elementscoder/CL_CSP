194a90f659a6e49e82957caae832d89b7c15487c
Elementscoder <caydon.lebaron@ucas-edu.net>
2026-09-09 13:52:42 -0600

    variables notes

---
+++ variables_notes.py
@@ -0,0 +1,9 @@
+# CL, Variables Notes
+
+name = input("Tell me your name: ") 
+animal = input("tell me a animal: ")
+
+print(name+animal)
+print("CPS is the best class!")
+print("It is taught by the coolest teacher!")
+print("Hello", name, "welcome to class!")

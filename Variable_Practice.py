daeb91e75971aa30edc9c223d1231f9dc0b765a0
Elementscoder <caydon.lebaron@ucas-edu.net>
2026-09-10 13:38:39 -0600

    Variable Practice

---
+++ Variable_Practice.py
@@ -0,0 +1,23 @@
+# CL, Variable Practice
+
+name = "Caydon"
+nine = "9"
+one_hundred_two = "102"
+morning_food = "rice pudding"
+fav_color = "purple"
+school_name = "West Lake"
+year = "2026"
+i_color = "green-blue"
+age = "15"
+fav_subject = "Math"
+
+print(name)
+print(nine)
+print(one_hundred_two)
+print(morning_food)
+print(fav_color)
+print(school_name)
+print(year)
+print(i_color)
+print(age)
+print(fav_subject)

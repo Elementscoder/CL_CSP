f91c9404551af67202babc1fc9e817d75b05b62f
Elementscoder <caydon.lebaron@ucas-edu.net>
2026-09-15 13:33:55 -0600

    fixing inputs

---
+++ memory_diagram.py
@@ -0,0 +1,28 @@
+# CL, Memory Diagram
+
+"Snippet 1"
+name = "Caydon"
+age = 15
+
+"Snippet 2"
+score = 10
+print(f"the location of {score} is {id(score)}")
+score = 25
+
+"Snippet 3"
+height = 5.11
+city = "Saratoga Springs"
+zip_code = 84048
+
+"Snippet 4"
+price1 = 10
+price2 = 10
+
+print(f"the location of {name} is {id(name)}")
+print(f"the location of {age} is {id(age)}")
+print(f"the location of {score} is {id(score)}")
+print(f"the location of {height} is {id(height)}")
+print(f"the location of {city} is {id(city)}")
+print(f"the location of {zip_code} is {id(zip_code)}")
+print(f"the location of {price1} is {id(price1)}")
+print(f"the location of {price2} is {id(price2)}")

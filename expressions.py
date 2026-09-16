884b1166257fa163f01557d4f25df421b16ef394
Elementscoder <caydon.lebaron@ucas-edu.net>
2026-09-16 13:53:01 -0600

    expressions

---
+++ expressions.py
@@ -0,0 +1,23 @@
+# CL, Integers, Floats and Expressions Note
+
+# Integers
+people = 18
+shoes = people * 2
+hand = people * 2
+
+# Float
+pi = 3.14159265358979323846264338327950288419716939937510582097494459230
+water = 60.8
+temp = 68.5
+price = 1.99
+
+# expression
+print(water*price)
+print(f"Your {water} gallons of water costs ${round(water*price,2)}")
+
+#modulo and integer division
+print(f"18/4= {18/4} which is {18//4} with a remainder of {18%4}")
+print(f"18/5= {18/5} which is {18//5} with a remainder of {18%5}")
+print(f"18/6= {18/6} which is {18//6} with a remainder of {18%6}")
+
+print(int(people*2*(5-2)/3))

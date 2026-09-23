088911e32c20e7fb6f6278f685a7214949b5d841
Elementscoder <caydon.lebaron@ucas-edu.net>
2026-09-23 13:53:36 -0600

    Password Strength Checker

--- password_checker.py
+++ password_checker.py
@@ -1,7 +1,29 @@
 # CL, Password Strength Checker
 
-password = input("Give me a password:\n")
+password = input("Give me a password:\n").strip()
+length = False
+upcase = False
+lowcase = False
+number = False
+symbol = False
 
-if len(password) >= 8: True
-if password.capitalize(): True
-if password.lower: True
+if len(password) >= 8:
+    length = True
+    print(f"Has at least eight characters: {length}")
+else:
+    print(f"Has at least eight characters: {length}")
+
+for letter in password:
+    if letter.isupper():
+        upcase = True
+    if letter.islower():
+        lowcase = True
+    if letter.isnumeric():
+        number = True
+    if letter in "!@#$%^&*":
+        symbol = True
+
+print(f"Has a uppercase letter: {upcase}")
+print(f"Has a losercase letter: {lowcase}")
+print(f"Has a number: {number}")
+print(f"Has a symbol: {symbol}")

5c9528b038613100b2dd76a59b295e1b1b7ff000
Elementscoder <caydon.lebaron@ucas-edu.net>
2026-09-24 13:11:38 -0600

    String

--- string.py
+++ string.py
@@ -1,10 +1,17 @@
 # CL, String Notes
-last_name = "Caydon" # surround with quotes
-first_name = "LeBaron" # surround with quotes
+
+last_name = "Caydon" 
+# surround with quotes
+first_name = "LeBaron" 
+# surround with quotes
+
 # concatenation => add two string together
-name = first_name + " " + last_name # puts two strings next to each other in the same string
+name = first_name + " " + last_name 
+# puts two strings next to each other in the same string
+
 # excape character lets the program ignore the next character in the string
-print(f'{name} told the class "You can\'t drive my car."') # f-string = formated string, 1. easy in sort variable, easier to specify having it print for the user
+print(f'{name} told the class "You can\'t drive my car."') 
+# f-string = formated string, 1. easy in sort variable, easier to specify having it print for the user
 
 user = input("Please tell me your name:\n").strip().title()
 

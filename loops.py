0d5d7479c2c738b3dc1e7cc20e66ef2e16174749
Elementscoder <caydon.lebaron@ucas-edu.net>
2026-09-24 13:52:27 -0600

    Loop Note

---
+++ loops.py
@@ -0,0 +1,20 @@
+# Cl, Loops Notes
+import random
+# code that will repeat over and over again
+count = 1
+
+while count <=10:
+    print(count)
+    count += 1
+
+goose = random.randint(1,11)
+ducks = 1
+
+while True:
+    print("duck")
+    if ducks == goose:
+        break
+    ducks += 1
+print("GOOSE!!!!")
+
+siblings = ["Macie", "Atalie", "Connor", "Chase", "Coda", "Jordon", "Kellie", "Landon", "Chelsey", "Kaleb", "Marilyn", "Jared", "Sadie"]

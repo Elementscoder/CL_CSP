4ecc91672d4036bba642ffeb0e9058182f9c7c9b
Elementscoder <caydon.lebaron@ucas-edu.net>
2026-09-25 15:40:10 -0600

    Number Guessing Game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,14 +1,25 @@
 import random
 
-print("I'm thinking of a number between 1 and 100. You have 6 tries to guess it!")
+print("I'm thinking of a integer between 1 and 100. You have 6 tries to guess it!")
 number = random.randint(1,100)
+guesses = 1
 
-while True:
-    guess = int(input("Guess #1:\n"))
-    if guess == number:
-        print("Correct! You guessed it in ")
-    elif guess > number:
+while guesses <=6:
+    while True:
+        try:
+            guess = int(input(f"Guess #{guesses}:\n"))
+            break
+        except:
+            print("That isn't an integer.")
+
+    if guess > number:
         print("Too high")
-    else:
+    elif guess < number:
         print("Too low")
+    else:
         break
+    guesses += 1
+if guesses <= 6:
+    print(f"Congratulations! You guessed it {guesses} tries, Good Job!")
+else:
+    print(f"You're out of guesses! The number was {number}.")
